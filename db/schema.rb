e407f91298d4ba056b2af7488ab039c86cd27516
Asiya <asiyakhan@Asiyas-MacBook-Pro.local>
2021-02-17 10:01:18 -0500

    create like and dislike abilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_225307) do
+ActiveRecord::Schema.define(version: 2021_02_17_061438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,15 @@ ActiveRecord::Schema.define(version: 2021_02_16_225307) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["account_id"], name: "index_subscriptions_on_account_id"
     t.index ["course_id"], name: "index_subscriptions_on_course_id"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.bigint "account_id"
+    t.bigint "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_votes_on_account_id"
+    t.index ["post_id"], name: "index_votes_on_post_id"
   end
 
 end
